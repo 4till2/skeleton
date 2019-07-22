af0f5c79ae02c9b43968a12f75c6fe35260844a4
yserkez <yosefserkez@gmail.com>
2019-07-21 22:46:08 -0700

    validation on client input

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const fs = require('./file-ops');
 
 const default_extension = '.js';
 const default_directory = './';
+const default_container = 'div';
+const default_type = 'class';
+
+const VALID_TYPES = ['class', 'function'];
+const VALID_EXTENSIONS = ['.js', '.html', '.css', '.txt'];
+const VALID_CONTAINER = ['div', 'span'];
+
+const VISUALIZE = false;
 
 function validateDirectory(directory) {
-  directory = directory || './';
+  directory = directory || default_directory;
   if (directory.slice(-1) != '/') directory = directory + '/';
   if (directory.slice(-2) === '//') directory = directory.slice(0, -1);
   if (!(fs.directoryExists(directory))) {
@@ -14,39 +22,55 @@ function validateDirectory(directory) {
   return directory;
 }
 
+function validateExtension(extension){
+  return (VALID_EXTENSIONS.includes(extension)) ? extension : default_extension;
+}
+
+function validateContainer(container){
+  return (VALID_CONTAINER.includes(container)) ? container : default_container;
+}
+
+function validateType(type){
+  return (VALID_TYPES.includes(type) ? type : default_type);
+}
+
+function validateName(name){
+  return (!fs.directoryExists(name))
+}
+
 //Create a file with a react template
-function createReactTemplate(name, directory, extension, classes, container, imports, type, visualize) {
+function createReactTemplate(element) {
   const template = new templates.ReactTemplate();
-  template.setName(name)
-  template.setClasses(classes)
-  template.setContainer(container);
-  template.setImport(imports)
-  template.setType(type);
-  template.setVisualize(visualize);
-  directory = validateDirectory(directory)
-  extension = extension || '.js';
-  fs.writeFile(directory + name + extension, template.template)
+  template.setName(element.name)
+  template.setClasses(element.classes)
+  template.setContainer(element.container);
+  template.setImport(element.imports)
+  template.setType(element.type);
+  template.setVisualize(element.visualize);
+  fs.writeFile(element.directory + element.name + element.extension, template.template)
 }
 
 function createSkeleton(outline) {
   if (!outline) {
     return
   }
+  let element = {
+    directory : validateDirectory(outline.directory),
+    name : validateName(outline.name),
+    extension : validateExtension(outline.extension),
+    classes : outline.classes,
+    container : validateContainer(outline.container),
+    imports : outline.children.length ? outline.children.map(function (e) {
+      return ({
+        name: validateName(e.name),
+        location: validateDirectory(e.directory) + e.name
+      })
+    }) : '',
+    type : validateType(outline.type),
+    visualize : VISUALIZE
+  }
 
-  let name = outline.name
-  let directory = outline.directory
-  let extension = '.js'
-  let classes = outline.classes
-  let container = ('div');
-  let imports = outline.children.length ? outline.children.map(function (e) {
-    return ({
-      name: e.name,
-      location: (e.directory || './') + e.name
-    })
-  }) : ''
-  let type = outline.type
-  let visualize = true;
-  createReactTemplate(name, directory, extension, classes, container, imports, type, visualize)
+  createReactTemplate(element)
   if (!outline.children) return
   outline.children.forEach(e => createSkeleton(e));
 }
